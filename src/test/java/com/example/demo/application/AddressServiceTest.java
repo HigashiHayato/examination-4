9e7a29f3af53ef6772722be97259e06e47206584
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-03-14 22:31:06 +0900

    全件取得における AddressServiceTest 実装(Red)

---
+++ src/test/java/com/example/demo/application/AddressServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.demo.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import com.example.demo.domain.Address;
+import com.example.demo.infrastructure.AddressMapper;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class AddressServiceTest {
+
+  private final Address ADDRESS =
+      new Address(1, "1000000", "東京都", "新宿区", "中落合");
+
+  @InjectMocks
+  private AddressService sut;
+
+  @Mock
+  private AddressMapper mapper;
+
+  @BeforeEach
+  public void setup() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void 全件取得により住所リストが返される() {
+    // setup
+    List<Address> addressList = List.of(
+        ADDRESS
+    );
+
+    when(mapper.selectAll()).thenReturn(addressList);
+
+    // execute
+    List<Address> actual = sut.retrieveAll();
+
+    // assert
+    assertEquals(addressList, actual);
+  }
+}
