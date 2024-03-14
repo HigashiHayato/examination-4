3dac8b227e8562458d2508edf3471f22bca98314
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-03-14 21:12:56 +0900

    設定ファイル作成

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -6,13 +6,6 @@ CREATE TABLE addresses (
     street_address varchar(100)
 );
 
-CREATE SEQUENCE IF NOT EXISTS ADDRESS_ID_SEQ
-    INCREMENT BY 1
-    MAXVALUE 9999999999
-    MINVALUE 1
-    START WITH 1
-;
-
-INSERT INTO addresses (id, zip_code, prefecture, city, street_address) VALUES (nextval('ADDRESS_ID_SEQ'), '1000000', '東京都', '千代田区', '以下に掲載がない場合');
-INSERT INTO addresses (id, zip_code, prefecture, city, street_address) VALUES (nextval('ADDRESS_ID_SEQ'), '1020072', '東京都', '千代田区', '飯田橋');
-INSERT INTO addresses (id, zip_code, prefecture, city, street_address) VALUES (nextval('ADDRESS_ID_SEQ'), '1500043', '東京都', '渋谷区', '道玄坂');
+INSERT INTO addresses VALUES (1, '1000000', '東京都', '千代田区', '以下に掲載がない場合');
+INSERT INTO addresses VALUES (2, '1020072', '東京都', '千代田区', '飯田橋');
+INSERT INTO addresses VALUES (3, '1500043', '東京都', '渋谷区', '道玄坂');
