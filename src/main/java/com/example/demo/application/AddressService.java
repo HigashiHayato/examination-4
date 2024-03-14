a732d7d591dd25d1ab1a29fe794ebd04c58a3ff8
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-03-14 22:37:21 +0900

    全件取得における AddressServiceTest 実装(Green)

---
+++ src/main/java/com/example/demo/application/AddressService.java
@@ -0,0 +1,26 @@
+package com.example.demo.application;
+
+import com.example.demo.domain.Address;
+import com.example.demo.infrastructure.AddressMapper;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Address に関するビジネスロジックを提供するサービスクラスです.
+ */
+@Service
+@RequiredArgsConstructor
+public class AddressService {
+
+  private final AddressMapper mapper;
+
+  /**
+   * 全ての Address を取得します.
+   *
+   * @return 全ての Address のリスト
+   */
+  public List<Address> retrieveAll() {
+      return mapper.selectAll();
+  }
+}
