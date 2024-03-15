a62760f61d0187eef3552a87b1e9647b7fa143d2
HigashiHayato <higashi_hayato@ims-sol.co.jp>
2024-03-15 13:53:56 +0900

    指定取得における AddressControllerTest の実装(Green)

--- src/main/java/com/example/demo/presentation/AddressController.java
+++ src/main/java/com/example/demo/presentation/AddressController.java
@@ -1,10 +1,12 @@
 package com.example.demo.presentation;
 
 import com.example.demo.application.AddressService;
+import com.example.demo.presentation.response.AddressResponse;
 import com.example.demo.presentation.response.AddressesResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,17 @@ public class AddressController {
   @ResponseStatus(HttpStatus.OK)
   public AddressesResponse get() {
     return AddressesResponse.of(addressService.retrieveAll());
+  }
+
+  /**
+   * 指定された ID の Address を取得するエンドポイントです.
+   *
+   * @param id 取得する Address の ID
+   * @return ResponseEntity オブジェクト
+   */
+  @GetMapping("v1/addresses/{id}")
+  @ResponseStatus(HttpStatus.OK)
+  public AddressResponse get(@PathVariable String id) {
+    return AddressResponse.convertToAddressResponse(addressService.retrieve(id));
   }
 }
